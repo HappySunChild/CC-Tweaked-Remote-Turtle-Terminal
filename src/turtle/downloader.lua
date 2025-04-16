afc5a98d7f974570be2f6e90ff4a52f452c998e1
HappySunChild <nothappysunchild@gmail.com>
2025-04-16 16:42:49 -0400

    add downloaders

---
+++ src/turtle/downloader.lua
@@ -0,0 +1,45 @@
+local ERR_DOWNLOAD_FAIL = "Unable to download file '%s'"
+local ERR_MAX_ATTEMPTS_REACH = "Unable to download %d file(s) after %d attempts.\nFiles:\n%s"
+
+local BASE_URL =
+	"https://raw.githubusercontent.com/HappySunChild/CC-Tweaked-Remote-Turtle-Terminal/refs/heads/master/src/turtle/"
+local FILES = {
+	"turtle.lua",
+	"commands.lua",
+	"main.lua",
+}
+
+local MAX_ATTEMPTS = 5
+
+local function downloadFiles(list, attempt)
+	attempt = attempt or 1
+
+	if attempt > MAX_ATTEMPTS then
+		printError(ERR_MAX_ATTEMPTS_REACH:format(#list, MAX_ATTEMPTS, table.concat(list, "\n")))
+
+		return
+	end
+
+	local retry = {}
+
+	for _, file in ipairs(list) do
+		if fs.exists(file) then
+			fs.delete(file)
+		end
+
+		local downloadUrl = BASE_URL .. file
+		local success = shell.execute("wget", downloadUrl, file)
+
+		if not success or not fs.exists(file) then
+			printError(ERR_DOWNLOAD_FAIL:format(file))
+
+			table.insert(retry, file)
+		end
+	end
+
+	if #retry > 0 then
+		downloadFiles(retry, attempt + 1)
+	end
+end
+
+downloadFiles(FILES)
